5926b5f1b3a65f2a8d900b411b7213717891c25f
CWagner23 <cwagner@wpi.edu>
2017-04-02 16:35:33 -0400

    Caleb's battery function file summary
    Caleb’s battery function file

---
+++ software_prototyping/main/Battery.c
@@ -0,0 +1,17 @@
+/*
+ * Battery.c
+ *
+ *  Created on: Apr 2, 2017
+ *      Author: calebwagner
+ */
+
+#include<msp430.h>
+#include "hw.h"
+
+int BatteryFunction(){
+
+
+    return battery_volatge;
+}
+
+
